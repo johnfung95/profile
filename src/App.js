86a1a7b0ce80aa7a32f677eeb7034c48a6fa3c06
john0810 <johnfung95@yahoo.com>
2023-02-15 11:25:20 +0800

    Added a Landing page with responsive design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Landing from "./components/Landing";
 import Header from "./components/Header";
 import Background from "./components/Background";
 
 function App() {
+  const [isEnter, setEnter] = useState(false);
+
+  const enterSiteHandler = () => {
+    setEnter(true);
+  };
+
   return (
     <main className="h-screen w-full">
-      {/* <Header /> */}
-      <Landing />
+      {isEnter && <Header />}
+      {!isEnter && <Landing enterSite={enterSiteHandler} />}
       <Background />
     </main>
   );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,17 +1,27 @@
 import React from "react";
 
-const Landing = () => {
+const Landing = ({ enterSite }) => {
   return (
-    <div className="h-full w-full flex flex-nowrap items-center justify-center text-white relative">
-      <div className="w-full flex justify-end items-center">
-        <img src="/logo512.png" alt="pic" className="w-full sticky"></img>
+    <div className="h-full w-full flex flex-col items-center justify-center text-white md:flex-row flex-nowrap  ">
+      <div className="w-full flex justify-center md:justify-end items-center">
+        <img src="/logo512.png" alt="pic" className="w-3/4"></img>
       </div>
-      <div className="w-full text-left pl-16">
-        <h1 className="motto ellipsis">My Motto....</h1>
-        <div className="pl-16">
+      <div className="w-full text-center mt-8 md:mt-0">
+        <h1 className="motto ellipsis text-center text-5xl md:text-7xl">
+          My Motto....
+        </h1>
+        <div className="mt-8">
           <p>My description</p>
           <p>Demo Paragraph Many Words.................</p>
           <p>Demo Paragraph Many Words.................</p>
+          <div className="text-center">
+            <button
+              onClick={enterSite}
+              className="half rounded-2xl italic w-32 h-8 mt-8"
+            >
+              Enter
+            </button>
+          </div>
         </div>
       </div>
     </div>
