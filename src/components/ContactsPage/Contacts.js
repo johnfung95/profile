bc196e2a203c20527a48ea16d2b16209a5c0d370
johnfung95 <johnfung95@yahoo.com>
2023-02-19 16:32:52 +0800

    Using satellite layer

--- src/components/ContactsPage/Contacts.js
+++ src/components/ContactsPage/Contacts.js
@@ -30,7 +30,7 @@ const Contacts = () => {
           </section>
           <section className="pl-32">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.9603913318338!2d114.1511611150079!3d22.27949018533439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3404007af32f0e7d%3A0x3a1cf553041b6c06!2sRobinson%20Heights!5e0!3m2!1sen!2shk!4v1676793284767!5m2!1sen!2shk"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.876232817914!2d114.1511611150079!3d22.27949018533439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3404007af32f0e7d%3A0x3a1cf553041b6c06!2sRobinson%20Heights!5e1!3m2!1sen!2shk!4v1676795513373!5m2!1sen!2shk"
               width="600"
               height="450"
               style={{ border: 0 }}
