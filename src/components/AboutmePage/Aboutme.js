d60c568fb380b3bfb90bd3bfe99869154b912273
johnfung95 <johnfung95@yahoo.com>
2023-02-23 21:42:52 +0800

    Adding a typewriter effect to the aboutme

--- src/components/AboutmePage/Aboutme.js
+++ src/components/AboutmePage/Aboutme.js
@@ -2,6 +2,7 @@ import React from "react";
 import ContentCard from "../UI/ContentCard";
 import Header from "../UI/Header";
 import Background from "../UI/Background";
+import Typewriter from "typewriter-effect";
 
 const Aboutme = () => {
   return (
@@ -10,9 +11,25 @@ const Aboutme = () => {
       <Header />
       <ContentCard>
         <main>
-          <div>
-            <p>Testing About me</p>
-          </div>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Hello World!")
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString("Another Text!")
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString("Howdy!")
+                .pauseFor(2500)
+                .deleteAll()
+                .typeString("Whats up")
+                .pauseFor(2500)
+                .deleteAll()
+                .start();
+            }}
+            options={{ autoStart: true, loop: true }}
+          />
         </main>
       </ContentCard>
     </React.Fragment>
