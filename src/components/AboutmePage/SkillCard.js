5634cce62dcc572987cc1a0b38cab4d32852c08c
john0810 <johnfung95@yahoo.com>
2023-04-04 16:13:50 +0800

    Add animation to skillcard

--- src/components/AboutmePage/SkillCard.js
+++ src/components/AboutmePage/SkillCard.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const SkillCard = ({ name, imgPath }) => {
   return (
-    <div className="w-48 h-full my-4">
-      <img
-        className="w-fit h-24 p-2 bg-zinc-700 m-auto rounded-lg"
-        src={imgPath}
-        alt={name}
-      />
+    <div className="w-48 h-full my-2">
+      <motion.div
+        className="w-full h-fit bg-zinc-700 rounded-lg"
+        initial={{ scale: 1 }}
+        animate={{ scale: 1.1 }}
+        transition={{
+          ease: "linear",
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+        <img className="w-fit h-24 p-2 m-auto " src={imgPath} alt={name} />
+      </motion.div>
       <p className="bold m-2 text-lg capitalize tracking-wide">{name}</p>
     </div>
   );
